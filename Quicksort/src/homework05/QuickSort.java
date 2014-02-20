e117f1308566a290567d6b85e55b3e61050dbfc4
headdetect <headdetective2@gmail.com>
2014-02-20 15:18:09 -0700

    Something

--- Quicksort/src/homework05/QuickSort.java
+++ Quicksort/src/homework05/QuickSort.java
@@ -7,43 +7,83 @@ public class QuickSort {
 	}
 
 	public void start() {
-
+		double[] fill = createRandomDoubleArray(10);
+		outputArray(fill);
+		quickSort(fill, 0, fill.length);
+		outputArray(fill);
 	}
 
-	public void quickSort(double[] array) {
-		int pivotIndex = array.length / 2;
+	private void outputArray(double[] data) {
+		System.out.print("{ ");
+		for(int i = 0; i < data.length; i++) {
+			System.out.print(data[i]);
+			if(i != data.length - 1) {
+				System.out.print(",");
+			}
+		}
+		System.out.println(" }");
+	}
+
+	private double[] createRandomDoubleArray(int len) {
+		double[] data = new double[len];
+		for(int i = 0; i < len; i++)
+			data[i] = Math.random();
+		return data;
+	}
+
+	public double[] quickSort(double[] array, int start, int end) {
+		int part = partition(array, start, end);
 		
-
+		if(array.length <= 2) {
+			return array;
+		}
+		
+		if(start < part - 1) {
+			quickSort(array, start, part - 1);
+		}
+		if(part < end) {
+			quickSort(array, part, end);
+		}
+		
+		return array;
 	}
-
-	public double[] partition(double[] array, int start, int end) {
-		double pivot = array[end];
+	
+	
+	public int partition(double[] data, int start, int end) {
+		
+		double pivot = data[end];
 		int left = start;
 		int right = end - 1;
+		
 
 		while (true) {
-			while (array[left] < pivot)
+
+			while (data[left] < pivot)
 				left++;
 
-			while (right > left && array[right] >= pivot)
+			while (right > left && data[right] >= pivot)
 				right--;
 
+			// Break just in case we updated the index //
 			if (left >= right)
 				break;
 
-			double temp = array[left];
-			array[left] = array[right];
-			array[right] = temp;
+			// Swapping left and right //
+			double temp = data[left];
+			data[left] = data[right];
+			data[right] = temp;
 
 			left++;
 			right--;
+			
 		}
 		
-		double temp = array[left];
-		array[left] = array[end];
-		array[end] = temp;
-
-		return array;
+		// Swapping left and right //
+		double temp = data[left];
+		data[left] = data[right];
+		data[right] = temp;
+		
+		return left;
 	}
 
 }
