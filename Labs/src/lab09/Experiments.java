12c8c59ff8e457b9f0c5f795d7f34cb3cf864b97
headdetect <headdetective2@gmail.com>
2014-04-12 13:38:53 -0600

    Finished this stupid ass assignment.

--- Labs/src/lab09/Experiments.java
+++ Labs/src/lab09/Experiments.java
@@ -62,10 +62,10 @@ public class Experiments
 	public void test2()
 	{
 		Map<Integer, Float> map = new TreeMap<Integer, Float>();
-		
+
 		int highestNumber = 5000;
 		int setCapacity = 200;
-		
+
 		for (int sample = 1; sample < 20; sample++)
 		{
 
@@ -173,10 +173,10 @@ public class Experiments
 						expectedSetSize--;
 					}
 				}
-				
-				if(!map.containsKey(set.size()))
-					map.put(set.size(), (float)set.getProbeCount());
-				
+
+				if (!map.containsKey(set.size()))
+					map.put(set.size(), (float) set.getProbeCount());
+
 				map.put(set.size(), (map.get(set.size()) + set.getProbeCount()) / 2);
 
 				set.resetProbeCount();
@@ -214,13 +214,355 @@ public class Experiments
 					break;
 			}
 		}
-		
+
 		float max = 0;
-		for(Entry<Integer, Float> var : map.entrySet()) {
+		for (Entry<Integer, Float> var : map.entrySet())
+		{
 			max = Math.max(var.getValue(), max);
 		}
-		for(Entry<Integer, Float> var : map.entrySet()) {
-			System.out.println(((float) var.getKey() / (float) setCapacity)  + "\t" + (var.getValue() / max));
+		for (Entry<Integer, Float> var : map.entrySet())
+		{
+			System.out.println(((float) var.getKey() / (float) setCapacity) + "\t" + (var.getValue() / max));
+		}
+	}
+
+	@Test
+	public void test3()
+	{
+		Map<Integer, Float> map = new TreeMap<Integer, Float>();
+
+		int highestNumber = 5000;
+		int setCapacity = 200;
+
+		for (int sample = 1; sample < 20; sample++)
+		{
+
+			// Determine the number of possible elements, the set capacity, and
+			// the number of tests.
+
+			int numberOfActions = 10 * sample;
+
+			// Create a boolean array that will indicate if each
+			// possible number is in our hash set.
+
+			boolean[] shouldBeInTheSet = new boolean[highestNumber];
+
+			// Create a counter to indicate how many elements are in the set.
+
+			int expectedSetSize = 0;
+
+			// Create the set to have the specified capacity.
+
+			SimpleHashSet set = new SimpleHashSet(setCapacity);
+
+			// Randomly add or remove numbers to the set and then
+			// verify the integrity of the set. This loop is weighted so that
+			// additions are more likely to happen at the start and
+			// removals are more likely to happen at the end.
+
+			Random r = new Random(8877614); // Used for repeatability
+
+			for (int actionCount = 0; actionCount < numberOfActions; actionCount++)
+			{
+				// Debug - print out the table. Comment out or uncomment as needed.
+				// set.debugTable();
+				// System.out.println("  " + set.getClusterMap());
+
+				// Select a random number element.
+
+				int number = r.nextInt(highestNumber);
+				NumberName element = new NumberName(number);
+
+				// Randomly determine an action to do with this element.
+
+				boolean doAdd = r.nextInt(numberOfActions) > actionCount;
+
+				if (doAdd || !doAdd)
+				{
+					try
+					{
+						// System.out.println("Adding " + element + " (hashes to location " + (Math.abs(element.hashCode()) % setCapacity) + ")");
+
+						// Attempt to add the element.
+
+						set.add(element);
+
+						// If the element was in the set, nothing should have happened.
+
+						if (!shouldBeInTheSet[number])
+						{
+							// The element was added. If the table was full, error.
+
+							if (expectedSetSize == setCapacity)
+							{
+								System.out.flush();
+								System.err.println("Set sizes disagree.  The table should have " + expectedSetSize + " elements in it, but an exception was not raised.");
+								System.err.flush();
+								return;
+							}
+
+							// Otherwise, increase the count.
+
+							shouldBeInTheSet[number] = true;
+							expectedSetSize++;
+						}
+					}
+					catch (TableFullException e)
+					{
+						// Verify that an exception should have been raised.
+
+						if (expectedSetSize != setCapacity)
+						{
+							System.out.flush();
+							System.err.println("Set sizes disagree.  The table should only have " + expectedSetSize + " elements in it, but it appears to have " + setCapacity
+									+ " elements in it.");
+							System.err.flush();
+							return;
+						}
+					}
+				}
+				else
+				{
+					// Delete an element.
+
+					// System.out.println("Removing " + element + " (hashes to location " + (Math.abs(element.hashCode()) % setCapacity) + ")");
+
+					// Attempt to remove the element.
+
+					set.delete(element);
+
+					// If the element was not in the set, nothing should have happened.
+
+					if (shouldBeInTheSet[number])
+					{
+						// The element was removed, mark it as removed.
+
+						shouldBeInTheSet[number] = false;
+						expectedSetSize--;
+					}
+				}
+
+				// Verify that the set only contains the elements that have been added but not removed.
+
+				boolean hasError = false;
+
+				for (int i = 0; i < highestNumber; i++)
+				{
+					NumberName n = new NumberName(i);
+
+					set.resetProbeCount();
+
+					boolean inTheSet = set.contains(n);
+
+					if (!map.containsKey(set.size()))
+						map.put(set.size(), (float) set.getProbeCount());
+
+					map.put(set.size(), (map.get(set.size()) + set.getProbeCount()) / 2);
+
+					set.resetProbeCount();
+
+					if (inTheSet && !shouldBeInTheSet[i])
+					{
+						System.out.flush();
+						System.err.println(n + " (hashes to location " + (Math.abs(n.hashCode()) % setCapacity) + ") appears to be in the set, but should not be.");
+						System.err.flush();
+						hasError = true;
+					}
+
+					if (!inTheSet && shouldBeInTheSet[i])
+					{
+						System.out.flush();
+						System.err.println(n + " (hashes to location " + (Math.abs(n.hashCode()) % setCapacity) + ") does not appear to be in the set, but should be.");
+						System.err.flush();
+						hasError = true;
+					}
+				}
+
+				// Stop testing if the validation fails.
+
+				if (hasError)
+					break;
+			}
+		}
+
+		float max = 0;
+		for (Entry<Integer, Float> var : map.entrySet())
+		{
+			max = Math.max(var.getValue(), max);
+		}
+		for (Entry<Integer, Float> var : map.entrySet())
+		{
+			System.out.println(((float) var.getKey() / (float) setCapacity) + "\t" + (var.getValue() / max));
+		}
+	}
+
+	@Test
+	public void test4()
+	{
+		Map<Integer, Float> map = new TreeMap<Integer, Float>();
+
+		int highestNumber = 5000;
+		int setCapacity = 200;
+
+		for (int sample = 1; sample < 20; sample++)
+		{
+
+			// Determine the number of possible elements, the set capacity, and
+			// the number of tests.
+
+			int numberOfActions = 10 * sample;
+
+			// Create a boolean array that will indicate if each
+			// possible number is in our hash set.
+
+			boolean[] shouldBeInTheSet = new boolean[highestNumber];
+
+			// Create a counter to indicate how many elements are in the set.
+
+			int expectedSetSize = 0;
+
+			// Create the set to have the specified capacity.
+
+			SimpleHashSet4 set = new SimpleHashSet4(setCapacity);
+
+			// Randomly add or remove numbers to the set and then
+			// verify the integrity of the set. This loop is weighted so that
+			// additions are more likely to happen at the start and
+			// removals are more likely to happen at the end.
+
+			Random r = new Random(8877614); // Used for repeatability
+
+			for (int actionCount = 0; actionCount < numberOfActions; actionCount++)
+			{
+				// Debug - print out the table. Comment out or uncomment as needed.
+				// set.debugTable();
+				// System.out.println("  " + set.getClusterMap());
+
+				// Select a random number element.
+
+				int number = r.nextInt(highestNumber);
+				NumberName element = new NumberName(number);
+
+				// Randomly determine an action to do with this element.
+
+				boolean doAdd = r.nextInt(numberOfActions) > actionCount;
+
+				if (doAdd || !doAdd)
+				{
+					try
+					{
+						// System.out.println("Adding " + element + " (hashes to location " + (Math.abs(element.hashCode()) % setCapacity) + ")");
+
+						// Attempt to add the element.
+
+						set.add(element);
+
+						// If the element was in the set, nothing should have happened.
+
+						if (!shouldBeInTheSet[number])
+						{
+							// The element was added. If the table was full, error.
+
+							if (expectedSetSize == setCapacity)
+							{
+								System.out.flush();
+								System.err.println("Set sizes disagree.  The table should have " + expectedSetSize + " elements in it, but an exception was not raised.");
+								System.err.flush();
+								return;
+							}
+
+							// Otherwise, increase the count.
+
+							shouldBeInTheSet[number] = true;
+							expectedSetSize++;
+						}
+					}
+					catch (TableFullException e)
+					{
+						// Verify that an exception should have been raised.
+
+						if (expectedSetSize != setCapacity)
+						{
+							System.out.flush();
+							System.err.println("Set sizes disagree.  The table should only have " + expectedSetSize + " elements in it, but it appears to have " + setCapacity
+									+ " elements in it.");
+							System.err.flush();
+							return;
+						}
+					}
+				}
+				else
+				{
+					// Delete an element.
+
+					// System.out.println("Removing " + element + " (hashes to location " + (Math.abs(element.hashCode()) % setCapacity) + ")");
+
+					// Attempt to remove the element.
+
+					set.delete(element);
+
+					// If the element was not in the set, nothing should have happened.
+
+					if (shouldBeInTheSet[number])
+					{
+						// The element was removed, mark it as removed.
+
+						shouldBeInTheSet[number] = false;
+						expectedSetSize--;
+					}
+				}
+
+				// Verify that the set only contains the elements that have been added but not removed.
+
+				boolean hasError = false;
+
+				for (int i = 0; i < highestNumber; i++)
+				{
+					NumberName n = new NumberName(i);
+
+					set.resetProbeCount();
+
+					boolean inTheSet = set.contains(n);
+
+					if (!map.containsKey(set.size()))
+						map.put(set.size(), (float) set.getProbeCount());
+
+					map.put(set.size(), (map.get(set.size()) + set.getProbeCount()) / 2);
+
+					set.resetProbeCount();
+
+					if (inTheSet && !shouldBeInTheSet[i])
+					{
+						System.out.flush();
+						System.err.println(n + " (hashes to location " + (Math.abs(n.hashCode()) % setCapacity) + ") appears to be in the set, but should not be.");
+						System.err.flush();
+						hasError = true;
+					}
+
+					if (!inTheSet && shouldBeInTheSet[i])
+					{
+						System.out.flush();
+						System.err.println(n + " (hashes to location " + (Math.abs(n.hashCode()) % setCapacity) + ") does not appear to be in the set, but should be.");
+						System.err.flush();
+						hasError = true;
+					}
+				}
+
+				// Stop testing if the validation fails.
+
+				if (hasError)
+					break;
+			}
+		}
+
+		float max = 0;
+		for (Entry<Integer, Float> var : map.entrySet())
+		{
+			max = Math.max(var.getValue(), max);
+		}
+		for (Entry<Integer, Float> var : map.entrySet())
+		{
+			System.out.println(((float) var.getKey() / (float) setCapacity) + "\t" + (var.getValue() / max));
 		}
 	}
 }
--- Labs/src/lab09/SimpleHashSet4.java
+++ Labs/src/lab09/SimpleHashSet4.java
@@ -1,33 +1,30 @@
 package lab09;
 
 /**
- * This class is a hash table implementation of a set. It is designed to support
- * simple hash table experiments. Each table has a fixed maximum size. Elements
- * can be added, deleted, or queried, but the number of elements in the set is
- * not available to the user. Also there is no mechanism for iterating through
- * the elements in a table. (These restrictions are imposed to prevent
- * inaccurate testing strategies.)
+ * This class is a hash table implementation of a set. It is designed to support simple hash table experiments. Each table has a fixed maximum size. Elements can be added, deleted,
+ * or queried, but the number of elements in the set is not available to the user. Also there is no mechanism for iterating through the elements in a table. (These restrictions are
+ * imposed to prevent inaccurate testing strategies.)
  * 
  * Element equality is determined using the .equals method.
  * 
  * The hash table is implemented using open addressing and linear probing.
  * 
- * In addition to the methods for accessing the set, this implementation keeps a
- * probe count that can be read and reset by the user. Every read of a location
- * in the hash table is considered a probe.
+ * In addition to the methods for accessing the set, this implementation keeps a probe count that can be read and reset by the user. Every read of a location in the hash table is
+ * considered a probe.
  * 
- * Finally, the user can request a cluster table. This is just a String object
- * that indicates which locations in the hash table are filled.
+ * Finally, the user can request a cluster table. This is just a String object that indicates which locations in the hash table are filled.
  * 
  * @author Peter Jensen
  * @version Spring 2014
  */
-public class SimpleHashSet4 {
+public class SimpleHashSet4
+{
 	// Instance variables.
 
-	private Object[] table;
+	private Node[] table;
 	private int capacity;
 	private int probeCount;
+	private int size;
 
 	/**
 	 * Builds a hash set with the specified maximum size.
@@ -35,192 +32,158 @@ public class SimpleHashSet4 {
 	 * @param capacity
 	 *            the size of the hash table
 	 */
-	public SimpleHashSet4(int capacity) {
+	public SimpleHashSet4(int capacity)
+	{
 		this.capacity = capacity;
-		this.table = new Object[capacity];
+		this.table = new Node[capacity];
 		this.probeCount = 0;
 	}
 
 	/**
-	 * Adds the specified element to the set. If the hash table already contains
-	 * an object equal to the specified object, the specified object will not be
-	 * added.
+	 * Adds the specified element to the set. If the hash table already contains an object equal to the specified object, the specified object will not be added.
 	 * 
 	 * @param element
 	 *            any object
 	 * @throws TableFullException
 	 *             if the hash table is full
 	 */
-	public void add(Object element) {
+	public void add(Object element)
+	{
 
-		if(element == null)
+		if (element == null)
 			return;
-		
+
 		int hash = Math.abs(element.hashCode()) % capacity;
 
-		for (int retry = 0; retry < capacity; retry++) {
-			int pos = (hash + retry) % capacity;
-			Object val = table[pos];
-			probeCount++;
-			if (val == null) {
-				table[pos] = element;
-				return;
+		Node n = table[hash];
+		if (n == null)
+		{
+			n = new Node(element);
+			size++;
+			table[hash] = n;
+		}
+		else
+		{
+			Node q = n;
+			Node prev = null;
+			while (true)
+			{
+				probeCount++;
+				prev = q;
+				q = q.next;
+				if (q == null)
+				{
+					q = prev;
+					break;
+				}
 			}
-			if (val.equals(element))
-				return;
+			if (q != null)
+				q.next = new Node(element);
+			else
+				n.next = new Node(element);
+			size++;
 		}
 
-		throw new TableFullException();
 	}
 
 	/**
-	 * Returns true if this element is in the set. The element is considered to
-	 * be in the set if it is equal to any object in the set.
+	 * Returns true if this element is in the set. The element is considered to be in the set if it is equal to any object in the set.
 	 * 
 	 * @param element
 	 *            any object
 	 * @return true if the element is in the set
 	 */
-	public boolean contains(Object element) {
+	public boolean contains(Object element)
+	{
 		if (element == null)
 			return false;
 
 		int hash = Math.abs(element.hashCode()) % capacity;
 
-		for (int retry = 0; retry < capacity; retry++) {
-			int pos = (hash + retry) % capacity;
-			Object val = table[pos];
-			probeCount++;
-
-			if(val == null)
-				return false;
-			
-			if (val.equals(element))
-				return true;
-			
+		Node n = table[hash];
+		if (n == null)
+		{
+			return false;
 		}
-
-		return false;
+		else
+		{
+			Node q = n;
+			while (q != null)
+			{
+				probeCount++;
+				if (q.value.equals(element))
+					return true;
+				q = q.next;
+			}
+			return false;
+		}
 	}
 
 	/**
-	 * Removes the specified element from the set. If the set does not contain
-	 * an object equal to the element, nothing happens.
+	 * Removes the specified element from the set. If the set does not contain an object equal to the element, nothing happens.
 	 */
-	public void delete(Object element) {
-		// Compute the hash code for the element, confine it to the table size.
-		// (A simple division-based hash function.)
+	public void delete(Object element)
+	{
+		if (element == null)
+			return;
 
 		int hash = Math.abs(element.hashCode()) % capacity;
 
-		// Try to locate the element at the hashed position. If it is not
-		// there, simply return.
-
-		int retry = 0;
-		for (; retry < capacity; retry++) {
-			// Determine what is in the hash table by probing it.
-
-			Object probed = table[(hash + retry) % capacity];
-			probeCount++;
-
-			// If that location in the table is empty, the element is not in the
-			// table.
-
-			if (probed == null)
-				return;
-
-			// If that location contains the element, terminate the loop.
-
-			if (probed.equals(element))
-				break;
-		}
-
-		// If the table was full, but the element was not found, return.
-
-		if (retry == capacity)
+		Node n = table[hash];
+		if (n == null)
+		{
 			return;
-
-		// Remove the element. This involves possibly moving other elements
-		// in the hash table to collapse the cluster.
-
-		int freePosition = (hash + retry) % capacity;
-
-		for (int count = 1; (freePosition + count) % capacity != hash; count++) {
-			// Determine what is in the hash table by probing it.
-
-			Object probed = table[(freePosition + count) % capacity];
-			probeCount++;
-
-			// If null, we're done collapsing the cluster.
-
-			if (probed == null)
-				break;
-
-			// If the probed object could legally occupy the free position, put
-			// it in the free position.
-			// To do this, determine the retry count that was used to place the
-			// probed element
-			// in the cache.
-
-			int probedHash = Math.abs(probed.hashCode()) % capacity;
-			int probedRetry = ((freePosition + count) - probedHash + capacity)
-					% capacity;
-
-			// If the probed retry is greater than or equal to our offset from
-			// the free
-			// position, move the element into the free position.
-
-			if (probedRetry >= count) {
-				table[freePosition] = probed;
-				freePosition = (freePosition + count) % capacity;
-				count = 0;
+		}
+		else
+		{
+			Node q = n;
+			while (q != null)
+			{
+				probeCount++;
+				if (q.value.equals(element))
+				{
+					n.next = q.next;
+					size--;
+					return;
+				}
+				q = q.next;
 			}
 		}
-
-		// Clear the free position.
-
-		table[freePosition] = null;
 	}
 
 	/**
-	 * Returns a string that indicates which elements of the table are used.
-	 * 
-	 * @return a string that shows table usage.
-	 */
-	public String getClusterMap() {
-		StringBuilder result = new StringBuilder(table.length);
-		for (Object o : table)
-			result.append(o == null ? '.' : 'X');
-
-		return result.toString();
-	}
-
-	/**
-	 * Returns the number of probes made to the table since the last probe count
-	 * resent.
+	 * Returns the number of probes made to the table since the last probe count resent.
 	 * 
 	 * @return the probe count
 	 */
-	public int getProbeCount() {
+	public int getProbeCount()
+	{
 		return probeCount;
 	}
 
 	/**
 	 * Resets the probe count to 0.
 	 */
-	public void resetProbeCount() {
+	public void resetProbeCount()
+	{
 		probeCount = 0;
+	}
+
+	public int size()
+	{
+		return size;
 	}
 
 	/**
 	 * A debugging method for printing out the table contents.
 	 */
-	public void debugTable() {
-		for (int i = 0; i < capacity; i++) {
+	public void debugTable()
+	{
+		for (int i = 0; i < capacity; i++)
+		{
 			System.out.print("Position " + i + ": " + table[i]);
 			if (table[i] != null)
-				System.out.println(" (hashes to location "
-						+ (Math.abs(table[i].hashCode()) % capacity) + ")");
+				System.out.println(" (hashes to location " + (Math.abs(table[i].hashCode()) % capacity) + ")");
 			else
 				System.out.println();
 		}
@@ -230,9 +193,23 @@ public class SimpleHashSet4 {
 	/**
 	 * An exception class for indicating the table is full.
 	 */
-	static class TableFullException extends RuntimeException {
-		TableFullException() {
+	static class TableFullException extends RuntimeException
+	{
+		TableFullException()
+		{
 			super("The hash table is full.");
 		}
+	}
+
+	static class Node
+	{
+		public Node next;
+		public Object value;
+
+		public Node(Object value)
+		{
+			this.value = value;
+		}
+
 	}
 }
