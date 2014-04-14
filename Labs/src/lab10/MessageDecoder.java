bd5c7f678acc21cb6284949eb08126939266b265
headdetect <headdetective2@gmail.com>
2014-04-14 14:26:14 -0600

    Lab 10

---
+++ Labs/src/lab10/MessageDecoder.java
@@ -0,0 +1,44 @@
+package lab10;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+public class MessageDecoder
+{
+
+	public static void main(String[] a)
+	{
+
+		try
+		{
+			File fi = new File("secret.txt");
+			FileInputStream stream = new FileInputStream(fi);
+			byte[] bytes = new byte[stream.available()];
+			stream.read(bytes);
+
+			// Bit[i][2] = Bit[i / 8][i % 8] 
+			
+			byte current = 0x00;
+			byte[] newBytes = new byte[bytes.length / 8];
+			
+			for (int i = 0; i < bytes.length; i++)
+			{
+				int bytePos = i / 8;
+				int bitPos = i % 8;
+				byte thisByte = bytes[i];
+				
+				if(bitPos == 0) current = 0x00;
+				current |= (((thisByte >> bitPos) & 1) << bitPos);
+				newBytes[bytePos] = (byte)(current & 0xFF);
+			}
+			
+			System.out.print(new String(newBytes, "ASCII"));
+
+			stream.close();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
